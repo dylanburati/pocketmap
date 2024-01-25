8c2f4623a8f6b45a9eabdbfe312cf8bfd4b7197d
Dylan Burati <dylanburati@protonmail.com>
2024-01-24 19:15:30 -0500

    borrow fxhash

---
+++ app/src/main/java/dev/dylanburati/App.java
@@ -0,0 +1,55 @@
+package dev.dylanburati;
+
+import dev.dylanburati.pocketmap.IntPocketMap;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Random;
+import java.util.TreeMap;
+
+public class App {
+  private static int genWordId(Random r) {
+    // Prob of returning x is proportional to (x+3.7) ** -1.01
+    // Similar distribution to English
+
+    // d/dx cdf = const * (x+3.7) ** -1.01
+    // cdf = const * ( [ -100 * (x+3.7) ** -0.01 ] - [ -100 * (3.7) ** -0.01 ] )
+    // cdf = 100 * const * [ (3.7) ** -0.01 - (x+3.7) ** -0.01 ]
+
+    // [ (3.7) ** -0.01 - (x+3.7) ** -0.01 ] = 0.01 * const * cdf
+    // x + 3.7 = (-0.01 * const * cdf + (3.7) ** -0.01) ** -100
+
+    // set maximum of x to 2**27 ->
+    // 2**27 = (-0.01 * const * 1 + (3.7**0.01)) ** -100
+    // import scipy.optimize
+    // scipy.optimize.minimize(lambda x: (27 + 100 * np.log2(-x/100 + (3.7 ** -0.01))) ** 2, 1).x[0]
+
+    // ignoring the lhs constant 3.7
+    return (int) Math.pow(-0.01 * 15.768233989819334 * r.nextDouble() + 0.9870018865063785, -100.0);
+  }
+
+  private static int wordcount(Map<String, Integer> m) {
+    byte[] alph = "pfscxkde".getBytes(StandardCharsets.US_ASCII);
+    byte[] wbuf = new byte[9];
+    Random r = new Random(0L);
+    for (int i = 0; i < 100_000_000; i++) {
+      int wlen = 0;
+      for (int wid = genWordId(r); wid > 0 && wlen < wbuf.length; wid = wid >> 3) {
+        wbuf[wlen++] = alph[wid & 7];
+      }
+      String word = new String(wbuf, 0, wlen, StandardCharsets.US_ASCII);
+      m.merge(word, 1, (v1, v2) -> v1 + v2);
+    }
+    System.out.println("Size: " + m.size());
+    return m.size();
+  }
+
+  public static void main( String[] args ) {
+    var m = switch (Optional.ofNullable(System.getProperty("switchPocketMapWith")).orElse("")) {
+      case "HashMap" -> new HashMap<String, Integer>();
+      default -> new IntPocketMap();
+    };
+    wordcount(m);
+  }
+}
--- pocketmap/src/main/java/dev/dylanburati/pocketmap/DefaultHasher.java
+++ pocketmap/src/main/java/dev/dylanburati/pocketmap/DefaultHasher.java
@@ -16,20 +16,45 @@ import java.nio.ByteBuffer;
 
   @Override
   public int hashBytes(byte[] keyContent) {
-    int h = 1;
-    for (int offset = keyContent.length - 1; offset >= 0; offset--) {
-      h = 31 * h + (int)keyContent[offset];
-    }
-    return h;
+    return this.hashImpl(keyContent, 0, keyContent.length);
   }
 
   @Override
   public int hashBuffer(ByteBuffer buf, int position, int length) {
-    int h = 1;
-    byte[] arr = buf.array();
-    for (int offset = position + length - 1; offset >= position; offset--) {
-      h = 31 * h + (int)arr[offset];
+    return this.hashImpl(buf.array(), position, length);
+  }
+
+  private static final int KEY32 = (int) 0x9e_37_79_b9L;
+
+  private int hashImpl(byte[] data, int position, int length) {
+    // https://github.com/cbreeden/fxhash/blob/master/lib.rs
+    int hash = 0;
+    int i = 0;
+    int word;
+    for ( ; i + 4 <= length; i += 4) {
+      word = (data[position+i] & 0xff)
+        | ((data[position+i+1] & 0xff) << 8)
+        | ((data[position+i+2] & 0xff) << 16)
+        | ((data[position+i+3] & 0xff) << 24);
+      hash = ((hash << 5) ^ word) * KEY32;
     }
-    return h;
+    switch (length - i) {
+      case 0:
+        return hash;
+      case 1:
+        word = data[position+i] & 0xff;
+        return ((hash << 5) ^ word) * KEY32;
+      case 2:
+        word = (data[position+i] & 0xff)
+          | ((data[position+i+1] & 0xff) << 8);
+        return ((hash << 5) ^ word) * KEY32;
+      case 3:
+        word = (data[position+i] & 0xff)
+          | ((data[position+i+1] & 0xff) << 8)
+          | ((data[position+i+2] & 0xff) << 16);
+        return ((hash << 5) ^ word) * KEY32;
+      default:
+        throw new RuntimeException();
+    }
   }
 }
